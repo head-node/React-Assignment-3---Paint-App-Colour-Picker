6377912aea358e679cd13f2d0df308155eee12fe
head-node <sudhanshuoff@gmail.com>
2021-03-03 21:49:09 +0530

    comments updated

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -3,9 +3,11 @@ import "../styles/Child.css"
 
 const Selection=(props)=>{
     const [currBg, setCurrBg] = React.useState({ background: "" });
-  const applyColor = () => {
-    props.applyColor(getColor);
-  };
+  const applyColor = () => {                  // here applyColor is a function defined in Selection.js
+                                              // and props.applyColor is the function passed to it as props
+    props.applyColor(getColor);                // we are basically passing getColor function to our props:- applyColor() 
+  };                                            // which act as updateSelectionStyle and nextBackground would be set to color 
+                                               // we just defined here what the function would do
   const getColor = (color) => {
     let newColor = color.background;
     setCurrBg({ background: newColor });
